df51ea0a7725ad185d0dc5912f7032672441b7c0
hson122 <hson122.learn@gmail.com>
2026-04-26 22:09:37 +0700

    update

--- train_new.py
+++ train_new.py
@@ -2,10 +2,25 @@ from share import *
 
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-from tutorial_dataset import MyDataset
+from tutorial_dataset_new import MyDataset
 from cldm.logger import ImageLogger
 from cldm.model import create_model, load_state_dict
+import argparse
 
+    # 1. Khởi tạo đối tượng đọc tham số
+parser = argparse.ArgumentParser(description="Dataset Loader cho ControlNet")
+    
+    # 2. Định nghĩa tham số --data_dir
+parser.add_argument(
+    '--data_dir', 
+    type=str, 
+    required=True, # Bắt buộc người dùng phải nhập tham số này
+    help='Đường dẫn tới thư mục chứa dataset (VD: ./training/fill50k)'
+)
+    
+    # 3. Phân tích các tham số được nhập từ terminal
+args = parser.parse_args()
+print(f"Đang tiến hành đọc dữ liệu từ: {args.data_dir}")
 
 # Configs
 resume_path = './models/control_sd15_ini.ckpt'
@@ -25,7 +40,9 @@ model.only_mid_control = only_mid_control
 
 
 # Misc
-dataset = MyDataset()
+dataset = MyDataset(data_dir=args.data_dir)
+print(f"Đã tải thành công {len(dataset)} mẫu dữ liệu.")
+
 dataloader = DataLoader(dataset, num_workers=0, batch_size=batch_size, shuffle=True)
 logger = ImageLogger(batch_frequency=logger_freq)
 trainer = pl.Trainer(gpus=1, precision=32, callbacks=[logger])
--- tutorial_dataset_new.py
+++ tutorial_dataset_new.py
@@ -45,22 +45,3 @@ class MyDataset(Dataset):
 
         return dict(jpg=target, txt=prompt, hint=source)
 
-if __name__ == '__main__':
-    # 1. Khởi tạo đối tượng đọc tham số
-    parser = argparse.ArgumentParser(description="Dataset Loader cho ControlNet")
-    
-    # 2. Định nghĩa tham số --data_dir
-    parser.add_argument(
-        '--data_dir', 
-        type=str, 
-        required=True, # Bắt buộc người dùng phải nhập tham số này
-        help='Đường dẫn tới thư mục chứa dataset (VD: ./training/fill50k)'
-    )
-    
-    # 3. Phân tích các tham số được nhập từ terminal
-    args = parser.parse_args()
-    
-    # 4. Khởi tạo Dataset với đường dẫn vừa nhận
-    print(f"Đang tiến hành đọc dữ liệu từ: {args.data_dir}")
-    dataset = MyDataset(data_dir=args.data_dir)
-    print(f"Đã tải thành công {len(dataset)} mẫu dữ liệu.")
